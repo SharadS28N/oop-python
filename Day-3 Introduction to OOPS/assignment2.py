332ead4c80825d7e7f1a96980fdd7818db65bec6
SharadS28N <sharad.bhandari222@gmail.com>
2026-04-06 14:16:08 +0545

    New Changes

---
+++ Day-3 Introduction to OOPS/assignment2.py
@@ -0,0 +1,9 @@
+class Cat:
+    pass
+
+class BankAccount:
+    name = "Kalpanik"
+    balance = 2500
+
+print("Bank Account Holder Name:", BankAccount.name)
+print("Bank Account Balance:", BankAccount.balance)
---
+++ Day-3 Introduction to OOPS/assignment3.py
@@ -0,0 +1,28 @@
+class Book:
+    title = "Around the world in 80 days"
+    author = "Jules Verne"
+    pages = 320
+
+
+class Movie:
+    title = "Inception"
+    director = "Christopher Nolan"
+    year = 2010
+
+
+class PhoneContact:
+    name="Sharad"
+    phone_number="+977-9841234567"
+    email="sharad@example.com"
+
+print("Book Title:", Book.title)
+print("Book Author:", Book.author)
+print("Book Pages:", Book.pages)
+
+print("Movie Title:", Movie.title)
+print("Movie Director:", Movie.director)
+print("Movie Year:", Movie.year)
+
+print("Contact Name:", PhoneContact.name)
+print("Contact Phone Number:", PhoneContact.phone_number)
+print("Contact Email:", PhoneContact.email)
